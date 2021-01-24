13a2fcd237800ed31cd1251ab30375352e266165
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2021-01-24 23:16:40 +0100

    debugging transient biot

--- sleep/fbb_DD/solid.py
+++ sleep/fbb_DD/solid.py
@@ -23,13 +23,13 @@ import ulfy  # https://github.com/MiroK/ulfy
 # 2) flux - constrols u.n
 #
 
-def solve_ale(W, f1, f2, eta_0, p_0, fbdries, bcs, parameters):
-    '''Return displacement, percolation velocity, pressure'''
+def solve_solid(W, f1, f2, eta_0, p_0, bdries, bcs, parameters):
+    '''Return displacement, percolation velocity, pressure and final time'''
     # NOTE: this is time dependent problem which we solve with 
     # parameters['dt'] for parameters['nsteps'] time steps and to 
     # update time in f1, f2 or the expressions bcs the physical time is
     # set as parameters['T0'] + dt*(k-th step)
-    mesh = V.mesh()
+    mesh = W.mesh()
 
     needed = set(bdries.array()) - set((0, ))    
     # Validate elasticity bcs
@@ -77,7 +77,7 @@ def solve_ale(W, f1, f2, eta_0, p_0, fbdries, bcs, parameters):
     # Material parameters
     kappa, mu, lmbda, alpha, s0 = (parameters[k] for k in ('kappa', 'mu', 'lmbda', 'alpha', 's0'))
     # For weak form also time step is needed
-    dt = parameters['dt']
+    dt = Constant(parameters['dt'])
 
     # Elasticity
     system = (2*mu*inner(sym(grad(eta)), sym(grad(phi)))*dx +
@@ -88,15 +88,14 @@ def solve_ale(W, f1, f2, eta_0, p_0, fbdries, bcs, parameters):
     # Darcy
     system += (1/kappa)*inner(u, v)*dx - inner(p, div(v))*dx
     # Mass conservation with backward Euler
-    system += (inner(s0*p, q)*dx + inner(alpha*div(eta), q)*dx + dt*inner(div(u), q)*dx
-               -dt*inner(f2, q)*dx
-               -inner(s0*p_0, q)*dx - inner(alpha*div(eta_0)*dx))
+    system += (inner(s0*(p-p_0)/dt, q)*dx + inner(alpha*div((eta-eta_0)/dt), q)*dx + inner(div(u), q)*dx
+               -inner(f2, q)*dx)
 
     # Handle natural bcs
     n = FacetNormal(mesh)
     ds = Measure('ds', domain=mesh, subdomain_data=bdries)
     # For elasticity
-    for tag, value in tractions_bcs:
+    for tag, value in traction_bcs:
         system += -inner(value, phi)*ds(tag)
     # For Darcy
     for tag, value in pressure_bcs:
@@ -133,7 +132,7 @@ def solve_ale(W, f1, f2, eta_0, p_0, fbdries, bcs, parameters):
         eta_0.assign(eta_h)
         p_0.assign(p_h)
         
-    return eta_h, u_h, p_h
+    return eta_h, u_h, p_h, T0
 
 
 def mms_solid(parameters):
@@ -150,54 +149,75 @@ def mms_solid(parameters):
     p = Function(Q)  # Pressure
     u = -kappa*grad(p)  # Define flux
 
-    sigma_p = lambda eta, p: 2*mu*sym(grad(eta)) + lmbda*div(eta)*Identity(len(eta)) - alpha*p*Idenity(len(eta))
+    sigma_p = lambda eta, p: 2*mu*sym(grad(eta)) + lmbda*div(eta)*Identity(len(eta)) - alpha*p*Identity(len(eta))
     
     f1 = -div(sigma_p(eta, p))
     
     # NOTE: we do not have temporal derivative but if we assume eta and p
     # have foo(x, y)*bar(t) then in mass conservation we will have
     # [s0*p + alpha*div(eta)]*dbar(t)
-    time = Function(S)
     
     # What we want to substitute
-    x, y, kappp_, mu_, lmbda_, alpha_, s0_ = sp.symbols('x y kappa mu lmbda alpha s0')
+    x, y, kappa_, mu_, lmbda_, alpha_, s0_ = sp.symbols('x y kappa mu lmbda alpha s0')
+    time_ = sp.Symbol('time')
     # Expressions
-    u_ = sp.Matrix([sp.sin(pi*(x + y))*sin(),
-                    sp.cos(pi*(x + y))])
-    
-    subs = {u: u_, kappa: kappa_}  # Function are replaced by symbols
+    eta_ = sp.Matrix([sp.sin(pi*(x + y)),
+                      sp.cos(pi*(x + y))])
 
-    as_expr = lambda t: ulfy.Expression(t, subs=subs, degree=4, kappa=kappa_value)
+    p_ = sp.cos(pi*(x-y))
+
+    # f2 = -(s0*p + alpha*div(eta)) + div(u)
+    f2 = div(u)
+    
+    subs = {eta: eta_, p: p_,
+            kappa: kappa_, mu: mu_, lmbda: lmbda_, alpha: alpha_, s0: s0_}
+
+    as_expr = lambda t: ulfy.Expression(t, subs=subs, degree=4,
+                                        kappa=parameters['kappa'],
+                                        mu=parameters['mu'],
+                                        lmbda=parameters['lmbda'],
+                                        alpha=parameters['alpha'],
+                                        s0=parameters['s0'],
+                                        time=0.0)
     
     # Solution
-    u_exact = as_expr(u)
+    eta_exact, u_exact, p_exact = map(as_expr, (eta, u, p))
     # Forcing
-    f = as_expr(f)
+    f1, f2 = as_expr(f1), as_expr(f2)
     #  4
     # 1 2
     #  3 so that
     normals = [Constant((-1, 0)), Constant((1, 0)), Constant((0, -1)), Constant((0, 1))]
-    fluxes = [as_expr(flux(u, n)) for n in normals]
+    tractions = [as_expr(dot(sigma_p(eta, p), n)) for n in normals]
 
-    return {'solution': u_exact,
-            'force': f,
-            'fluxes': fluxes}
+    return {'solution': (eta_exact, u_exact, p_exact),
+            'force': (f1, f2),
+            'tractions': tractions}
            
 # --------------------------------------------------------------------
 
 if __name__ == '__main__':
 
-    kappa_value = 1E0
-    data = mms_ale(kappa_value=kappa_value)
+    parameters = {'kappa': Constant(1),
+                  'mu': Constant(1),
+                  'lmbda': Constant(1),
+                  'alpha': Constant(1),
+                  's0': Constant(1)}
+    data = mms_solid(parameters)
     
-    u_exact  = data['solution']
-    forcing = data['force']
-    fluxes = dict(enumerate(data['fluxes'], 1))
+    eta_exact, u_exact, p_exact  = data['solution']
+    f1, f2 = data['force']
+    tractions = dict(enumerate(data['tractions'], 1))
 
-    # Taylor-Hood
-    Velm = VectorElement('Lagrange', triangle, 1)
+    Eelm = VectorElement('Lagrange', triangle, 2)
+    Velm = FiniteElement('Raviart-Thomas', triangle, 1)
+    Qelm = FiniteElement('Discontinuous Lagrange', triangle, 0)
 
-    parameters = {'kappa': Constant(kappa_value)}
+    Welm = MixedElement([Eelm, Velm, Qelm])
+
+    parameters['dt'] = 0.001
+    parameters['nsteps'] = 5
+    parameters['T0'] = 0.
     for n in (4, 8, 16, 32, 64):
         mesh = UnitSquareMesh(n, n)
         # Setup similar to coupled problem ...
@@ -207,14 +227,39 @@ if __name__ == '__main__':
         CompiledSubDomain('near(x[1], 0)').mark(bdries, 3)
         CompiledSubDomain('near(x[1], 1)').mark(bdries, 4)
 
-        # Top bottom are prescribed displacement, and on sides we put
-        # stresses
-        bcs = {'dirichlet': [(3, u_exact), (4, u_exact)],
-               'neumann': [(1, fluxes[1]), (2, fluxes[2])]}
+        # Reset time
+        for things in data.values():
+            for thing in things:
+                thing.time = 0.
+        
+        # Elasticity: displacement bottom, traction for rest
+        # Darcy: pressure bottom and top, flux on sides
+        bcs = {'elasticity': {'displacement': [(3, eta_exact), (1, eta_exact), (2, eta_exact), (4, eta_exact)],
+                              #'traction': [(1, tractions[1]), (2,tractions[2]), (4, tractions[4])]},
+                              },
+               'darcy': {'pressure': [(1, p_exact), (2, p_exact), (3, p_exact), (4, p_exact)],
+                         #'flux': [(1, u_exact), (2, u_exact)]}}
+                         }}
 
-        V = FunctionSpace(mesh, Velm)
-        uh = solve_ale(V, f=forcing, bdries=bdries, bcs=bcs,
-                       parameters=parameters)
+        # Get the initial conditions
+        E = FunctionSpace(mesh, Eelm)
+        Q = FunctionSpace(mesh, Qelm)
+
+        eta_0 = interpolate(u_exact, E)
+        p_0 = interpolate(p_exact, Q)
+
+        W = FunctionSpace(mesh, Welm)
+        ans = solve_solid(W, f1, f2, eta_0, p_0, bdries=bdries, bcs=bcs,
+                          parameters=parameters)
+
+        eta_h, u_h, p_h, time = ans
+        eta_exact.time, u_exact.time, p_exact.time = (time, )*3
         # Errors
-        eu = errornorm(u_exact, uh, 'H1', degree_rise=2)
-        print('|u-uh|_1', eu)
+        e_eta = errornorm(eta_exact, eta_h, 'H1', degree_rise=2)        
+        e_u = errornorm(u_exact, u_h, 'Hdiv', degree_rise=2)
+        e_p = errornorm(p_exact, p_h, 'L2', degree_rise=2)        
+        print('|eta-eta_h|_1', e_eta, '|u-uh|_div', e_u, '|p-ph|_0', e_p)
+
+        for i, (x, y) in enumerate(zip((eta_h, u_h, p_h), (eta_exact, u_exact, p_exact))):
+            File('x%d_true.pvd' % i) << interpolate(y, x.function_space())
+            File('x%d_numeric.pvd' % i) << x
